aa141bc5d54b6f1a74f9e85eb00d7b2cb52c3a1e
ANIL MADHIRA <anil@aztik.com>
2021-07-17 14:23:55 +0530

    updated code

---
+++ src/main/java/com/jwttest/myapp/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.jwttest.myapp.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@EnableConfigurationProperties
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/com/jwttest/myapp/controller/MyAppController.java
@@ -0,0 +1,26 @@
+package com.jwttest.myapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class MyAppController {
+
+
+	@GetMapping("/cert")
+	public @ResponseBody ResponseEntity < String > getCert() {
+        return new ResponseEntity < String > ("Response from GET", HttpStatus.OK);
+    }
+	
+	@PostMapping("/cert/add")
+	public @ResponseBody ResponseEntity < String > addCert() {
+        return new ResponseEntity < String > ("Response from POST", HttpStatus.OK);
+    }
+
+}
